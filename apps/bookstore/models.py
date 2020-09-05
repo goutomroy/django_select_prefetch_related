9bb02d4809e0af9528de97da5586516ba316e3be
goutom <goutom.sust.cse@gmail.com>
2020-09-05 12:48:00 +0600

    Changed user reference

--- apps/bookstore/models.py
+++ apps/bookstore/models.py
@@ -1,10 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.conf import settings
 
 
 class Publisher(models.Model):
     name = models.CharField(max_length=300)
-    owner = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='publisher')
+    owner = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='publisher')
 
     def __str__(self):
         return self.name
